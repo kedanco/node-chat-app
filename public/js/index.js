36094db0885dd2d8926404455a93d6ebe17e6600
kedanco <kelvinchowjunjie120@hotmail.com>
2018-12-16 22:39:40 +0800

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -30,13 +30,17 @@ socket.on("newLocationMessage", function(message) {
 jQuery("#message-form").on("submit", function(e) {
 	e.preventDefault();
 
+	let messageTextBox = jQuery("[name=message]");
+
 	socket.emit(
 		"createMessage",
 		{
 			from: "User",
-			text: jQuery("[name=message]").val()
+			text: messageTextBox.val()
 		},
-		function() {}
+		function() {
+			messageTextBox.val("");
+		}
 	);
 });
 
@@ -46,14 +50,18 @@ locationButton.on("click", function(e) {
 		return alert("Geolocation not supported by your browser.");
 	}
 
+	locationButton.attr("disabled", "disabled").text("Sending Location...");
+
 	navigator.geolocation.getCurrentPosition(
 		function(position) {
+			locationButton.removeAttr("disabled").text("Send Location");
 			socket.emit("createLocationMessage", {
 				latitude: position.coords.latitude,
 				longitude: position.coords.longitude
 			});
 		},
 		function(err) {
+			locationButton.removeAttr("disabled").text("Send Location");
 			alert("Unable to fetch location");
 		}
 	);
